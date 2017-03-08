30455718fe64ab806649c04e721bf985c1798bd1
lonclesam <samuelpoirier5@gmail.com>
2017-03-08 14:27:22 +0100

    image plus stils

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    cv::Mat inputImage = cv::imread("/home/daniel/Images/Wallpapers/27239.jpg");
+    cv::Mat inputImage = cv::imread("/home/sam/Images/emily.jpg");
     cv::imshow("Display Image", inputImage);
 }
 
