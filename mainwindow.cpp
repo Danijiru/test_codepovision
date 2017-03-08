e881953bb6677c16d0de67a9486ed1ca107a2fa2
lonclesam <samuelpoirier5@gmail.com>
2017-03-08 14:36:03 +0100

    des truc écris

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,10 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    ui->setupUi(this);::c
     cv::Mat inputImage = cv::imread("/home/sam/Images/emily.jpg");
     cv::imshow("Display Image", inputImage);
+    std::cou << "bonjour on choux";
 }
 
 MainWindow::~MainWindow()
